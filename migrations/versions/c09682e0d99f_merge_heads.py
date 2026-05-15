d7cbb34eb84f0cd2ff49dc792556c552f5f67e13
nhuntkn <nhuntkn@gmail.com>
2026-05-15 19:11:36 +0800

    Pull from main

---
+++ migrations/versions/c09682e0d99f_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge_heads
+
+Revision ID: c09682e0d99f
+Revises: 8a1f8e384e1e, c65a30a162e4
+Create Date: 2026-05-15 19:07:43.617805
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c09682e0d99f'
+down_revision = ('8a1f8e384e1e', 'c65a30a162e4')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
